d54740a5309e161d353e22a8e0e033599eb057c4
Digus Nascimento <rdigus@gmail.com>
2020-08-07 01:08:52 -0300

    teste do commit

---
+++ src/main/java/br/com/startcode/testemaven01/ClasseDeTeste.java
@@ -0,0 +1,10 @@
+package br.com.startcode.testemaven01;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class ClasseDeTeste {
+	
+	public static void main(String[] args) {
+		System.out.println(StringUtils.capitalize("Digus"));
+	}
+}
